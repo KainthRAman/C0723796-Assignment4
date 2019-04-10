d399d16f9c29b03ccb48f932f1a54a0c80c5ea88
visualstudio <visualstudio@122-PC35>
2019-04-10 09:10:05 -0400

    Second commit

--- C0723796Raman/Program.cs
+++ C0723796Raman/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,26 @@ namespace C0723796Raman
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+        public void Run() { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader(@"U:\Users\723796/beowulf.text"))
+            {
+                int counter = 0;
+            }
+
+        }
+
     }
 }
+
+    
+   
+
+    
