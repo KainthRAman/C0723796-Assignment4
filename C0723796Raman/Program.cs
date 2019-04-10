86541f7a5cb5c8251b1d954068654e43f6a128ac
visualstudio <visualstudio@122-PC35>
2019-04-10 10:16:46 -0400

    8th commit in sec A

--- C0723796Raman/Program.cs
+++ C0723796Raman/Program.cs
@@ -11,46 +11,57 @@ namespace C0723796Raman
     class Program
     {
         ArrayList Beowulf;
+        int LineCount = 0;
         static void Main(string[] args)
+
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
+            p.Run();
+
         }
         public void Run() { this.ReadTextFiles(); }
         public void ReadTextFiles()
         {
-            using (StreamReader file = new StreamReader(@"U:\Users\723796/beowulf.text"))
+            //Read file using StreamReader.Reads file line by line
+            using (StreamReader file = new StreamReader(@"U:/Users/723796/beowulf"))
             {
                 int counter = 0;
-                String ln;
-                while ((ln = file.ReadLine()) !=null)
+                string ln;
+                while ((ln = file.ReadLine()) != null)
                 {
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
                 }
                 file.Close();
-                Console.WriteLine($"File Has {counter} lines.");
-            }
+                Console.WriteLine($"File has {counter} lines.");
 
+            }
         }
         public int FindNumberOfBlankSpaces(string line)
         {
-
             int countletters = 0;
             int countSpaces = 0;
             foreach (char c in line)
             {
-                if ( char.IsLetter(c)) { countletters++; }
+                if (char.IsLetter(c)) { countletters++; }
                 if (char.IsWhiteSpace(c)) { countSpaces++; }
-           
             }
             return countSpaces;
-        }
 
+        }
+        public void count()
+        {
+            string line;
+            TextReader reader = new StreamReader((@"U:/Users/723796/beowulf"));
+            while ((line = reader.ReadLine()) != null)
+            {
+                LineCount++;
+            }
+            reader.Close();
+            Console.WriteLine(LineCount);
+        }
     }
 }
 
-    
-   
 
-    
